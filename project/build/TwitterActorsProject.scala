5ed14495d2ad95ce64a9f42a9631153691f0da78
Matt Freels <matt@freels.name>
2011-02-16 22:50:36 -0800

    scope test dependencies

--- project/build/TwitterActorsProject.scala
+++ project/build/TwitterActorsProject.scala
@@ -5,8 +5,8 @@ import com.twitter.sbt.{SubversionPublisher, StandardProject}
 
 class TwitterActorsProject(info: ProjectInfo) extends StandardProject(info) with SubversionPublisher {
   val specs     = buildScalaVersion match {
-    case "2.7.7" => "org.scala-tools.testing" % "specs" % "1.6.2.1"
-    case _ => "org.scala-tools.testing" %% "specs" % "1.6.5"
+    case "2.7.7" => "org.scala-tools.testing" % "specs" % "1.6.2.1" % "test"
+    case _ => "org.scala-tools.testing" % "specs_2.8.0" % "1.6.5" % "test"
   }
 
   override def disableCrossPaths = false
