fa7252aacee926bec9c8d83443288b6f1a830072
Matt Freels <matt@freels.name>
2012-02-15 12:16:56 -0800

    add 2.9.1 support

--- project/build/TwitterActorsProject.scala
+++ project/build/TwitterActorsProject.scala
@@ -4,11 +4,12 @@ import com.twitter.sbt.{SubversionPublisher, StandardProject}
 
 
 class TwitterActorsProject(info: ProjectInfo) extends StandardProject(info) with SubversionPublisher {
-  val specs     = buildScalaVersion match {
+  val specs = buildScalaVersion match {
     case "2.7.7" => "org.scala-tools.testing" % "specs" % "1.6.2.1" % "test"
-    case _ => "org.scala-tools.testing" % "specs_2.8.0" % "1.6.5" % "test"
+    case "2.8.0" => "org.scala-tools.testing" %% "specs" % "1.6.5"  % "test"
+    case _       => "org.scala-tools.testing" %% "specs" % "1.6.9"  % "test"
   }
 
   override def disableCrossPaths = false
-  override def subversionRepository = Some("http://svn.local.twitter.com/maven-public")
+  override def subversionRepository = Some("https://svn.twitter.biz/maven-public")
 }
